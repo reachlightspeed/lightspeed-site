5b7168adb85be26aa1f97dfa99a084f0b2b30802
Dan Klammer <danklammer@gmail.com>
2020-03-01 02:54:53 -0500

    Polish up those styles, baby!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,68 @@
 module.exports = {
   theme: {
-    extend: {}
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      body: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+    extend: {
+      colors: {
+        spacepurple: {
+          default: '#3b5ee2',
+          dark: '#1E2A61',
+        },
+      },
+      borderWidth: {
+        default: '1px',
+        '0': '0',
+        '2': '2px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
+      },
+      lineHeight: {
+        none: 1,
+        tight: 1.125,
+        snug: 1.25,
+        normal: 1.5,
+        relaxed: 1.625,
+        loose: 2,
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      }
+    },
   },
-  variants: {},
+  variants: {
+    borderColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    borderWidth: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
   plugins: []
 }
