0a3eb00df47782194543808b0841fa3866f11ac9
tomhanlon <tomhanlon@gmail.com>
2020-05-10 16:26:16 -0500

    Save user setting for dark/light mode

--- src/site/_includes/js/darkmode.js
+++ src/site/_includes/js/darkmode.js
@@ -1,5 +1,29 @@
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+function enableDarkMode(){
   document.documentElement.classList.add("mode-dark");
-} else {
+}
+
+function disableDarkMode(){
   document.documentElement.classList.remove("mode-dark");
+}
+
+// check first for user dark mode setting
+if (localStorage.getItem('userDarkModeSetting')) {
+  // enable color scheme based on manual user setting
+  localStorage.getItem('userDarkModeSetting') === 'dark' ? enableDarkMode() : disableDarkMode();
+} 
+// if user has not set dark mode setting on our site, check for system preferences
+else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  // apply dark mode if system pref contains dark 
+  enableDarkMode();
+} else {
+  // show regular site (light mode)
+  disableDarkMode();
+}
+
+// function triggered by click of moon icon anchor tag
+function toggleDarkMode(){
+  // manually toggle dark mode or light mode depending on current setting
+  document.documentElement.classList.toggle('mode-dark');
+  // set dark or light based on toggle setting
+  localStorage.setItem('userDarkModeSetting', document.documentElement.classList.contains('mode-dark') ? 'dark' : 'light');
 }
