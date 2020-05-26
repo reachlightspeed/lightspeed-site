c608a6a5d98cdd47a9d540df54ff5b77434a86f3
Dan Klammer <danklammer@gmail.com>
2020-05-26 11:47:09 -0400

    Added additional spacing units

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,6 +74,9 @@ module.exports = {
       },
       spacing: {
         "-3px": "-3px",
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
       },
       maxWidth: {
         "7xl": "80rem",
