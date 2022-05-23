76087807e8d0c8af7a18f6664d8c549c74a34bf2
Dan Klammer <danklammer@gmail.com>
2022-05-23 01:05:04 -0400

    Updated colors, Updated to Tailwind 3 custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,21 @@ module.exports = {
       sans: ['Avenir Next', 'Avenir', 'Segoe UI', 'Helvetica', 'helvetica', 'sans-serif'],
     },
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
       colors: {
         lightspeed: {
           green: '#85F8A2',
           blue: '#4A84BE',
           purple: '#7060B4',
+          night: '#0F153A',
+          midnight: '#0A0C2A',
         }
+      },
+      fontSize: {
+        'md': '.9375rem',
       }
     }
-  },
-  plugins: [],
+  }
 }
